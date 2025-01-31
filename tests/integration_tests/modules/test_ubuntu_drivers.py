062bc2bea9b0e1e9be2605bb0dfb53eca22b776e
a-dubs <alecwarren19@gmail.com>
2025-01-31 12:33:36 -0500

    test(oracle): fix test_ubuntu_drivers_installed
    Since the cc_ubuntu_drivers module runs the ubuntu-drivers command with
    the "--gpgpu" flag, only a small subset of nvidia packages are installed
    which do not include "linux-modules-nvidia" that the test originally
    asserted were installed. Now, just look for any nvidia-*-server packages
    that are installed.

--- tests/integration_tests/modules/test_ubuntu_drivers.py
+++ tests/integration_tests/modules/test_ubuntu_drivers.py
@@ -1,10 +1,17 @@
+import logging
 import re
+from typing import Optional
 
 import pytest
 
-from tests.integration_tests.clouds import IntegrationCloud
+from tests.integration_tests.clouds import (
+    IntegrationCloud,
+    IntegrationInstance,
+)
 from tests.integration_tests.integration_settings import PLATFORM
 from tests.integration_tests.util import verify_clean_boot, verify_clean_log
+
+logger = logging.getLogger(__name__)
 
 USER_DATA = """\
 #cloud-config
@@ -13,15 +20,52 @@ drivers:
         license-accepted: true
 """
 
-# NOTE(VM.GPU2.1 is not in all availability_domains: use qIZq:US-ASHBURN-AD-1)
 
-
-@pytest.mark.adhoc  # Expensive instance type
 @pytest.mark.skipif(PLATFORM != "oci", reason="Test is OCI specific")
 def test_ubuntu_drivers_installed(session_cloud: IntegrationCloud):
-    with session_cloud.launch(
-        launch_kwargs={"instance_type": "VM.GPU2.1"}, user_data=USER_DATA
-    ) as client:
+    """
+    Test the installation of NVIDIA drivers on an OCI instance.
+
+    This test checks that the ubuntu-drivers module installs NVIDIA drivers
+    as expected on an OCI instance.
+
+    This test launches its own instance so that it can ensure that a GPU
+    instance type is used. The "VM.GPU.A10.1" instance type is used because
+    it is the most widely available GPU instance type on OCI.
+
+    Additionally, in case there is limited availability of GPU instances, this
+    test launches an instance outside the normal context manager used in other
+    tests. This is so that if the instance fails to launch, the test can be
+    marked as xfail rather than just failing.
+
+    Test Steps:
+    1. Launch a GPU instance with the user data that installs NVIDIA drivers.
+    2. Verify that the cloud-init log is clean.
+    3. Verify that the instance boots cleanly.
+    4. Verify that the NVIDIA drivers are installed as expected.
+    """
+    client: Optional[IntegrationInstance] = None
+
+    # create context manager that will call client.cleanup() at the end
+    class Cleanup:
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            if client:
+                logger.info(
+                    "Automatically cleaning up self-launched GPU instance"
+                )
+                client.destroy()
+            if exc_type is not None:
+                pytest.xfail(f"Failed to launch instance: {exc_val}")
+
+    with Cleanup():
+        client = session_cloud.launch(
+            launch_kwargs={"instance_type": "VM.GPU.A10.1"},
+            user_data=USER_DATA,
+        )
+
         log = client.read_from_file("/var/log/cloud-init.log")
         verify_clean_log(log)
         verify_clean_boot(client)
@@ -31,9 +75,7 @@ def test_ubuntu_drivers_installed(session_cloud: IntegrationCloud):
         )
         result = client.execute("dpkg -l | grep nvidia")
         assert result.ok, "No nvidia packages found"
-        assert re.search(
-            r"ii\s+linux-modules-nvidia-\d+-server", result.stdout
-        ), (
+        assert re.search(r"ii\s+nvidia.*-\d+-server", result.stdout), (
             f"Did not find specific nvidia drivers packages in:"
             f" {result.stdout}"
         )
